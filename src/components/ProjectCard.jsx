a0fb83673d66dc636812a9e9ac2a2dab32860bab
KingMenes <menaibr96@gmail.com>
2023-07-28 18:49:26 -0400

    Fixed project cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -9,11 +9,11 @@ function ProjectCard({ item }) {
   return (
     <>
       <a href={item.link} target="_blank">
-        <div className="img-box w-[700px] h-[300px] lg2:w-auto mx-auto flex justify-center items-center relative overflow-hidden rounded-3xl">
+        <div className="img-box w-[700px] h-[300px] lg2:w-auto mx-auto flex justify-center items-center relative overflow-hidden rounded-3xl bg-[#1a67b3] bg-opacity-5">
           <img
             src={item.img}
             alt=""
-            className="flex-shrink-0 min-w-full min-h-full"
+            className="object-cover min-w-full min-h-full"
           />
           <div className="p flex flex-col justify-center items-center absolute top-[-100%] left-0 h-full w-full text-center bg-[#1a67b3] bg-opacity-80 p-4">
             <h1 className="font-bold text-3xl">{item.title}</h1>
